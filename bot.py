d5a0d0eefa1e99a74075b464187def818eea204b
Apon77 <khalakuzzamanapon5@gmail.com>
2021-01-17 00:16:49 +0600

    welcome

---
+++ bot.py
@@ -0,0 +1,14 @@
+#pip install pyTelegramBotAPI
+import telebot
+import time
+
+bot_token = '1336436573:AAFpaGsPLEc90A9LBObk6kSXDvjrySmQH14'
+
+bot = telebot.TeleBot(token=bot_token)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Welcome!')
+
+bot.polling()
+    
